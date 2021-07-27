fa9cddb527a3d0832b4d23b5a74212186cd0a5c7
Andrey-optimist <83393527+Andrey-optimist@users.noreply.github.com>
2021-07-28 02:18:47 +0300

    Add files via upload

---
+++ priv2/src/navigation_goals1.cpp
@@ -0,0 +1,55 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+
+
+#define NUM_GOALS 3
+double goals[NUM_GOALS][4] = {
+	{1, -2.0, -0.5, 0.866}, // x y z w
+	{-1.0, -2.0, 1, 0}, 
+	{0.0, 0.0, 0.5, 0.866},	
+};
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "navigation_goals");
+	MoveBaseClient ac("move_base", true);
+
+	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
+
+	move_base_msgs::MoveBaseGoal goal;
+
+	goal.target_pose.header.frame_id = "map";
+	goal.target_pose.header.stamp = ros::Time::now();
+
+	for(int i = 0; i < NUM_GOALS; ++i)
+	{
+		goal.target_pose.pose.position.x = goals[i][0];
+		goal.target_pose.pose.position.y = goals[i][1];
+		goal.target_pose.pose.orientation.z = goals[i][2];
+		goal.target_pose.pose.orientation.w = goals[i][3];
+		
+
+		ROS_INFO("Sending goal: (%f; %f)", goals[i][0], goals[i][1]);
+		ac.sendGoal(goal);
+
+		ac.waitForResult();
+		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+			ROS_INFO("Goal achieved!");
+
+
+		ros::Duration(1.0).sleep();
+	}
+	ROS_INFO("Done.");
+	return 0;
+}
+
+
+
+
